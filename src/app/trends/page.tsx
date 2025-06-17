5857a354eba76ce634300d12b3db253e40ed8b0c
HarryKim24 <tl9434@naver.com>
2025-06-17 13:23:20 +0900

    div 스타일 수정

--- src/app/trends/page.tsx
+++ src/app/trends/page.tsx
@@ -6,13 +6,13 @@ import TopVolume from "@/components/trends/TopVolume";
 const TrendsPage = () => {
   return (
     <div className="px-4 lg:px-20 max-w-screen-xl mx-auto text-neutral-100">
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 items-stretch">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 items-stretch pb-4 lg:pb-0">
         <div className="lg:col-span-2 flex flex-col gap-6 h-full">
           <ExchangeRates />
           <TopPerformance />
         </div>
 
-        <div className="flex flex-col gap-6 h-full">
+        <div className="flex flex-col gap-6 h-full min-w-[320px]">
           <WeeklyTopRise />
           <TopVolume />
         </div>
