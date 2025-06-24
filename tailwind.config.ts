a17bfaa58d98187730cc79b24be0eafa9e6f8da9
HarryKim24 <tl9434@naver.com>
2025-06-24 13:35:26 +0900

    gradient 색상 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,9 +22,9 @@ const config: Config = {
         "main-gradient": "linear-gradient(to bottom, #5731a8, #c637b3, #f27567, #ffb84c)",
         "second-gradient": "linear-gradient(90deg, #f08c6c, #f6b072, #fbc987, #f6b072, #f08c6c)",
         'chart-gradient': 'linear-gradient(to bottom,#267187, #24345c)',
-        'trends-gradient': 'linear-gradient(to bottom,#8e5a67, #5a3a56, #3c2441)',
-        'portfolio-gradient': 'linear-gradient(to bottom,#8383c3, #7b5ea8, #4f3a6c)',
-        'setting-gradient': 'linear-gradient(to bottom,rgb(1, 2, 2), #2d4b4a,  #1f3337)',
+        'trends-gradient': 'linear-gradient(to bottom, #8e5a67, #5a3a56, #3c2441)',
+        'portfolio-gradient': 'linear-gradient(to bottom, #8383c3, #7b5ea8, #4f3a6c)',
+        'setting-gradient': 'linear-gradient(to bottom, #2d4b4a, #1f3337)',
       },
       fontFamily: {
         sans: ['"Noto Sans"', "sans-serif"],
