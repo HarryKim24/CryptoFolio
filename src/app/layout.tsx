da80e8fc760cba18cc71329ee91552c058fddc51
HarryKim24 <tl9434@naver.com>
2025-07-20 14:05:01 +0900

    Google Fonts 로딩 최적화: preconnect 및 <link> 사용으로 FCP/LCP 개선

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/nav/Navbar";
@@ -9,7 +10,8 @@ export const metadata: Metadata = {
     "CryptoFolio는 차트 조회, 트렌드 분석, 포트폴리오 만들기 제공하는 웹 사이트입니다.",
   openGraph: {
     title: "CryptoFolio | 실시간 암호화폐 포트폴리오",
-    description: "암호화폐 가격을 실시간으로 조회하고, 트렌드를 분석하여 나만의 맞춤형 포트폴리오를 만들어보세요.",
+    description:
+      "암호화폐 가격을 실시간으로 조회하고, 트렌드를 분석하여 나만의 맞춤형 포트폴리오를 만들어보세요.",
     url: "https://crypto-folio-harrykim24.vercel.app/",
     siteName: "CryptoFolio",
     locale: "ko_KR",
@@ -27,6 +29,12 @@ export default function RootLayout({
     <html lang="ko">
       <head>
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
       </head>
       <body>
         <SessionClientLayout>
