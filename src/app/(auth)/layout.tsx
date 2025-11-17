d78e584331a5393ea231cee26050ee277c385810
HarryKim24 <tl9434@naver.com>
2025-11-17 15:49:13 +0900

    Auth 레이아웃 및 컴포넌트 리팩터링

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -3,12 +3,12 @@ import ChildrenWrapper from "../../components/auth/ChildrenWrapper";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-main-gradient px-16 pt-20">
+    <main className="min-h-screen flex items-center justify-center bg-main-gradient px-4 md:px-16 pt-20">
       <div className="flex flex-col items-center w-full max-w-[480px] min-w-[220px]">
         <TitleHeader />
         <ChildrenWrapper>{children}</ChildrenWrapper>
       </div>
-    </div>
+    </main>
   );
 };
 
